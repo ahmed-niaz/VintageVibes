f0d826e892566af634be1d9e587674994ba5dd85
ahmed-niaz <niazahmed.an@gmail.com>
2024-03-04 04:17:05 +0600

    complete the latest section

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const post = async () => {
     );
     const data = await resp.json();
     const reviews = data.posts;
-    // console.log(reviews);
+    //  console.log(reviews);
     displayPost(reviews);
   } catch (error) {
     console.error("Error fetching data:", error);
@@ -16,7 +16,7 @@ const displayPost = (reviews) => {
   const informationCard = document.getElementById(`information-card`);
   // console.log(reviews);
   reviews.forEach((review) => {
-    console.log(review);
+    // console.log(review);
     const cardContainer = document.createElement(`div`);
     cardContainer.innerHTML = `
         <div class="card bg-[#F3F3F5] mb-5">
@@ -69,4 +69,64 @@ const displayPost = (reviews) => {
   });
 };
 
+const latestPost = async () => {
+  try {
+    const resp = await fetch(
+      `https://openapi.programming-hero.com/api/retro-forum/latest-posts`
+    );
+    const data = await resp.json();
+    const profiles= data;
+    //  console.log(profiles);
+     displayLatestPost(profiles);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+
+ const displayLatestPost = (profiles) => {
+  const latestPostCard = document.getElementById(`latest-post`)
+  profiles.forEach((profile)=>{
+     console.log(profile);
+    const latestPostContainer = document.createElement(`div`);
+    latestPostContainer.innerHTML = `
+    <div class="card w-full bg-base-100 shadow-xl pb-5">
+            <figure class="px-10 pt-10">
+              <img
+                src="${profile.cover_image}"
+                class="rounded-xl"
+              />
+            </figure>
+            <div class="flex gap-4 pl-10 mt-4">
+              <img src="./assets/icons/calendar.svg" alt="calendar" />
+              <p>${profile.author.posted_date}</p>
+            </div>
+            <h2 class="pl-10 w-3/4 mt-4 font-extrabold">
+             ${profile.title}
+            </h2>
+            <p class="pl-10 w-11/12 mt-4">
+              ${profile.description}
+            </p>
+            <div class="pl-10 flex gap-4 mt-4">
+              <div class="avatar">
+                <div
+                  class="w-12 rounded-full"
+                >
+                  <img
+                    src="${profile.profile_image}"
+                  />
+                </div>
+              </div>
+              <div>
+                <h2 class="font-extrabold">${profile.author.name}</h2>
+                <h4>${profile.author.designation}</h4>
+              </div>
+            </div>
+          </div>
+    `
+    latestPostCard.appendChild(latestPostContainer)
+  })
+ }
+
 post();
+latestPost();
