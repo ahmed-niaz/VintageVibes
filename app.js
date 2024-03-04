0e6089a15f33a158d28099d29385c892e1bb9a61
ahmed-niaz <niazahmed.an@gmail.com>
2024-03-04 20:03:28 +0600

    fully responsive

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const post = async () => {
     const reviews = data.posts;
     //  console.log(reviews);
     displayPost(reviews);
+    markAsRead(reviews);
   } catch (error) {
     console.error("Error fetching data:", error);
   }
@@ -21,9 +22,10 @@ const displayPost = (reviews) => {
     cardContainer.innerHTML = `
         <div class="card bg-[#F3F3F5] mb-5">
               <div class="card-body" >
-                <div class="flex">
-                  <div class="basis-1/5">
-                    <div class="avatar online">
+                <div class="flex flex-col lg:flex-row justify-center items-center
+                ">
+                  <div class="basis-1/5 px-6">
+                    <div class="avatar ${review.isActive ? 'online': 'offline'}">
                       <div class="w-24 rounded-2xl">
                         <img
                           src="${review.image}"
@@ -33,31 +35,33 @@ const displayPost = (reviews) => {
                   </div>
                   <div class="basis-4/5">
                     <div class="flex">
-                      <p>#${review.category}</p>
-                      <p>Author:${review.author.name}</p>
+                      <p class="font-bold">#${review.category}</p>
+                      <p class="font-bold">Author:${review.author.name}</p>
                     </div>
-                    <h4 class="mt-4 font-extrabold">${review.title}</h4>
+                    <h4 class="mt-4 font-extrabold ">${review.title}</h4>
                     <h6 class="mt-2">
                       ${review.description}
                     </h6>
                     <p class="border-t-2 border-dashed w-full border-[#12132D40] mt-2"></p>
                     <div class="flex justify-between mt-6">
                       <div class="flex gap-16">
-                        <div class="flex gap-4">
+                        <div class="flex gap-4 items-center flex-col lg:flex-row">
                           <img src="./assets/icons/text.svg" alt="Text" />
                           <p>${review.comment_count}</p>
                         </div>
-                        <div class="flex gap-4">
+                        <div class="flex gap-4 items-center flex-col lg:flex-row">
                           <img src="./assets/icons/watch.svg" alt="watch" />
                           <p>${review.view_count}</p>
                         </div>
-                        <div class="flex gap-4">
+                        <div class="flex gap-4 items-center flex-col lg:flex-row">
                           <img src="./assets/icons/time.svg" alt="time" />
                           <p>${review.posted_time}min</p>
                         </div>
                       </div>
                       <div>
-                        <img src="./assets/icons/mail-box.svg" alt="" />
+                      <button onclick="markAsRead()" id='markAsBtn'>
+                      <img src="./assets/icons/mail-box.svg" alt="" />
+                      </button>
                       </div>
                     </div>
                   </div>
@@ -67,7 +71,34 @@ const displayPost = (reviews) => {
         `;
     informationCard.appendChild(cardContainer);
   });
+  
 };
+
+
+const markAsRead = (reviews) =>{
+  const markAsReadContainer = document.getElementById(`mark-as-read`);
+  reviews.forEach(review => {
+    // console.log(review);
+    const markAsReadDynamic = document.createElement(`div`);
+    markAsReadDynamic.innerHTML = `
+      <div class="card bg-white text-black mb-8">
+        <div class="card-body">
+          <div class="flex justify-between items-center">
+            <div>
+              <h2 class='font-bold'>${review.title}</h2>
+            </div>
+            <div class="flex gap-6 flex-col lg:flex-row">
+              <img src="./assets/icons/watch.svg" alt="watch" />
+              <p>${review.view_count}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+    markAsReadContainer.appendChild(markAsReadDynamic);
+  });
+}
+
 
 const latestPost = async () => {
   try {
@@ -75,19 +106,18 @@ const latestPost = async () => {
       `https://openapi.programming-hero.com/api/retro-forum/latest-posts`
     );
     const data = await resp.json();
-    const profiles= data;
+    const profiles = data;
     //  console.log(profiles);
-     displayLatestPost(profiles);
+    displayLatestPost(profiles);
   } catch (error) {
     console.error("Error fetching data:", error);
   }
-}
+};
 
-
- const displayLatestPost = (profiles) => {
-  const latestPostCard = document.getElementById(`latest-post`)
-  profiles.forEach((profile)=>{
-     console.log(profile);
+const displayLatestPost = (profiles) => {
+  const latestPostCard = document.getElementById(`latest-post`);
+  profiles.forEach((profile) => {
+    console.log(profile);
     const latestPostContainer = document.createElement(`div`);
     latestPostContainer.innerHTML = `
     <div class="card w-full bg-base-100 shadow-xl pb-5">
@@ -99,7 +129,7 @@ const latestPost = async () => {
             </figure>
             <div class="flex gap-4 pl-10 mt-4">
               <img src="./assets/icons/calendar.svg" alt="calendar" />
-              <p>${profile.author.posted_date}</p>
+              <p>${profile.author.posted_date || "No publish Date"}</p>
             </div>
             <h2 class="pl-4 lg:pl-10 w-full lg:w-3/4 mt-4 font-extrabold">
              ${profile.title}
@@ -119,14 +149,15 @@ const latestPost = async () => {
               </div>
               <div >
                 <h2 class="font-extrabold">${profile.author.name}</h2>
-                <h4>${profile.author.designation}</h4>
+                <h4>${profile.author.designation  || "unknown"}</h4>
               </div>
             </div>
           </div>
-    `
-    latestPostCard.appendChild(latestPostContainer)
-  })
- }
+    `;
+    latestPostCard.appendChild(latestPostContainer);
+  });
+};
 
 post();
 latestPost();
+
